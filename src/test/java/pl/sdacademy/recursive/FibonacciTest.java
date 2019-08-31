b1290b5ddfd0dea825f8338bff8a6dd13c1649b1
Przydan <patryk@przydanek.com>
2019-08-31 14:17:01 +0200

    factorial methods iteration and recursive

--- src/test/java/pl/sdacademy/recursive/FibonacciTest.java
+++ src/test/java/pl/sdacademy/recursive/FibonacciTest.java
@@ -6,19 +6,19 @@ import org.junit.Test;
 public class FibonacciTest {
 
     @Test
-    public voirecursive()on() {
+    public void iteration() {
         // given
         Fibonacci fibonacci = new Fibonacci();
         //when
 
         //then
-        Assert.assertEquals(0,fibonacci.recursive(0));
-        Assert.assertEquals(1,fibonacci.recursive(1));
-        Assert.assertEquals(1,fibonacci.recursive(2));
-        Assert.assertEquals(2,fibonacci.recursive(3));
-        Assert.assertEquals(3,fibonacci.recursive(4));
-        Assert.assertEquals(5,fibonacci.recursive(5));
-        Assert.assertEquals(8,fibonacci.recursive(6));
+        Assert.assertEquals(0,fibonacci.iteration(0));
+        Assert.assertEquals(1,fibonacci.iteration(1));
+        Assert.assertEquals(1,fibonacci.iteration(2));
+        Assert.assertEquals(2,fibonacci.iteration(3));
+        Assert.assertEquals(3,fibonacci.iteration(4));
+        Assert.assertEquals(5,fibonacci.iteration(5));
+        Assert.assertEquals(8,fibonacci.iteration(6));
     }
 
     @Test
